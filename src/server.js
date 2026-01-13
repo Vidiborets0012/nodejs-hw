ecdc6fc690be0a804e990786bc5549fc405a3486
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-13 19:42:21 +0200

    add standard Middleware and Middleware for logging

--- src/server.js
+++ src/server.js
@@ -1,8 +1,31 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: '5mb' }));
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
