4536a98510141d69edaac3e1b88ebec3e3e0ee9d
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-13 19:29:01 +0200

    implement GET /notes and GET /notes/:noteId routes

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
 app.listen(PORT, () => {
