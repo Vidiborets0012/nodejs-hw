1bf0f7144f95913d0fab28dda411af904fffd569
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-17 23:31:23 +0200

    add PATCH /notes/:noteId route

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -32,3 +32,17 @@ export const deleteNote = async (req, res, next) => {
 
   res.status(200).json(note);
 };
+
+export const updateNote = async (req, res, next) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
