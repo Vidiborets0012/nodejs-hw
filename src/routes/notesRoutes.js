73610538ca050d412ca71e46fa3212e69ef324d4
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-17 23:05:30 +0200

    add GET /notes/:noteId route

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { getAllNotes } from '../controllers/notesController';
+import { getAllNotes, getNoteById } from '../controllers/notesController.js';
 
 const router = Router();
 router.get('/notes', getAllNotes);
+router.get('/notes/:noteId', getNoteById);
 
 export default router;
