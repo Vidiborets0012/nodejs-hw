26bee7dbf7ad74ae9f4a6f879a66a9e8062d5431
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-25 23:21:53 +0200

    add ID validation (noteIdSchema)

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -8,13 +8,16 @@ import {
   updateNote,
 } from '../controllers/notesController.js';
 
-import { getAllNotesSchema } from '../validations/notesValidation.js';
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+} from '../validations/notesValidation.js';
 
 const router = Router();
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 router.patch('/notes/:noteId', updateNote);
 
 export default router;
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,5 +1,16 @@
 import { Joi, Segments } from 'celebrate';
 import { TAGS } from '../constants/tags.js';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
