b423d58a7944c0392c6ea19f2933fccb93ec693b
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-25 23:48:46 +0200

    add validation for PATCH (updateNoteSchema)

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -32,3 +32,16 @@ export const createNoteSchema = {
       .optional(),
   }),
 };
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).optional(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+  }).min(1),
+};
