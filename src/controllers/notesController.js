bb30ccae4d51637559135a3b27a2c8b3ec75005f
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-25 22:36:24 +0200

    add tag filtering

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,11 +2,15 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const { page = 1, perPage = 10 } = req.query;
+  const { page = 1, perPage = 10, tag } = req.query;
 
   const skip = (page - 1) * perPage;
 
   const notesQuery = Note.find();
+
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
 
   const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,8 +1,12 @@
 import { Joi, Segments } from 'celebrate';
+import { TAGS } from '../constants/tags.js';
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1),
     perPage: Joi.number().integer().min(5).max(20),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
   }),
 };
