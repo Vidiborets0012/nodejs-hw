dd215053d7a76ccd5f06e6645e616496d5e8e140
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-25 21:17:14 +0200

    add validation error handling from the celebrate package to the server.js file

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import helmet from 'helmet';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -19,6 +20,9 @@ app.use(express.json({ limit: '5mb' }));
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+// обробка помилок від celebrate (валідація)
+app.use(errors());
 
 app.use(errorHandler);
 
