253f748b6bf8e5603fba47a7f93e8e97c0a25e30
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-27 07:30:24 +0200

    set default values for page and perPage (getAllNotesSchema)

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -14,8 +14,8 @@ export const noteIdSchema = {
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer().min(1),
-    perPage: Joi.number().integer().min(5).max(20),
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string()
       .valid(...TAGS)
       .optional(),
