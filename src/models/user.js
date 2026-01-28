40adaa05e09209ae06bf7cb0259a05ec5a0ac54e
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-28 21:20:45 +0200

    add the toJSON method to the userSchema schema, add the pre(‘save’) hook

--- src/models/user.js
+++ src/models/user.js
@@ -20,4 +20,16 @@ const userSchema = new Schema(
   { timestamps: true, versionKey: false },
 );
 
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const User = model('User', userSchema);
