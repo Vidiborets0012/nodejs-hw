1511fcc6cf89303605dd6da3039386172d599f79
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-29 13:58:16 +0200

    create and add the createSession helper function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession } from '../services/auth.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -17,6 +19,8 @@ export const registerUser = async (req, res) => {
     password: hashedPassword,
   });
 
+  const newSession = await createSession(newUser._id);
+
   res.status(201).json(newUser);
 };
 
@@ -32,6 +36,10 @@ export const loginUser = async (req, res) => {
   if (!isValidPassword) {
     throw createHttpError(401, 'Invalid credentials');
   }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
 
   res.status(200).json(user);
 };
