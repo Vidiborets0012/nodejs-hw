858107af5d3cfd8b611e53be9358ae34d4b8eec2
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-02 12:42:51 +0200

    install handlebars, add a letter template (src/templates/reset-password-email.html) and connect Handlebars in the controller (src/controllers/authController.js)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,9 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
@@ -111,12 +114,20 @@ export const requestResetEmail = async (req, res) => {
     { expiresIn: '15m' },
   );
 
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
   try {
     await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
-      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+      html,
     });
   } catch {
     throw createHttpError(
